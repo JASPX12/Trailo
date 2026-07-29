d8f438480a77071477e512cde1363b2a2e717257
Juan Hoyos <juanandreshoyosocampo@gmail.com>
2026-07-29 15:44:06 -0500

    Modificación de log in, registrarse, cambiar contraseña y conexión con la base de datos

--- backend/database.py
+++ backend/database.py
@@ -1,10 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-# 1. Tu URI de conexión a PostgreSQL
-URL_BASE_DATOS = "postgresql://postgres:postgres@localhost:5432/cines"
+# URL de conexión directa a tu proyecto en Supabase
+URL_BASE_DATOS = "postgresql://postgres.pumgpjfvogqgmpdurnhp:qwBLq70Mg8Ju3Jpu@aws-0-us-east-1.pooler.supabase.com:5432/postgres"
 
-engine = create_engine(URL_BASE_DATOS)
+# Conectamos con el engine. pool_pre_ping=True asegura que las conexiones caídas se descarten
+engine = create_engine(URL_BASE_DATOS, pool_pre_ping=True)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 def get_db():
